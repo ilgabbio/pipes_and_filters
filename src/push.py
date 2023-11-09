a01a4de7f30c51a40ef34e388980e28711315ccc
Gabriele Lombardi <gabriele.lombardi@argo.vision>
2023-11-09 21:20:08 +0100

    feat: push

---
+++ src/push.py
@@ -0,0 +1,113 @@
+from __future__ import annotations
+import cv2
+import numpy as np
+from .shapes import Rect, Landmarks
+from .tools import flatten
+from abc import ABC, abstractmethod
+
+
+def main():
+    # Initializing the pipeline:
+    source = Source().then(Preprocessor()).then(FacesDetector()).then(LandmarksDetector()).then(Display())
+
+    # Running the pipeline:
+    Runner(source).run()
+
+
+class Step(ABC):
+    def __init__(self):
+        self._next: Step | None = None
+    
+    def push(self, whiteboard: dict | None):
+        self.close() if self is None else self._op(whiteboard)
+
+        if self._next is not None:
+            self._next.push(whiteboard)
+
+    def then(self, step: Step) -> Step:
+        if self._next is not None:
+            self._next.then(step)
+        else:
+            self._next = step
+        return self
+    
+    @abstractmethod
+    def _op(self, frame: dict) -> dict | None:
+        pass
+
+    def close(self):
+        pass
+
+
+class Runner:
+    def __init__(self, source: Step):
+        self._source = source
+
+    def run(self):
+        while cv2.waitKey(1) != 27:
+            self._source.push({"shapes": {}})
+
+
+class Source(Step):
+    def __init__(self):
+        super().__init__()
+        self._cap = cv2.VideoCapture(0)
+        if not self._cap.isOpened():
+            raise IOError("Cannot open webcam")
+
+    def _op(self, frame: dict):
+        _, image = self._cap.read()
+        frame["image"] = image
+
+    def close(self):
+        self._cap.release()
+
+
+class Preprocessor(Step):
+    def _op(self, frame):
+        gray = cv2.cvtColor(frame["image"], cv2.COLOR_BGR2GRAY)
+        frame["gray"] = gray
+
+
+class FacesDetector(Step):
+    def __init__(self):
+        super().__init__()
+        self._detector = cv2.CascadeClassifier(
+            cv2.data.haarcascades + 'haarcascade_frontalface_default.xml',
+        )
+
+    def _op(self, frame):
+        faces = tuple(map(Rect,
+            self._detector.detectMultiScale(frame["gray"], 1.1, 4)
+        ))
+        frame["shapes"]["faces"] = faces
+
+
+class LandmarksDetector(Step):
+    def __init__(self) -> None:
+        super().__init__()
+        self._detector = cv2.face.createFacemarkLBF()
+        self._detector.loadModel('models/lbfmodel.yaml')
+
+    def _op(self, frame):
+        faces = frame["shapes"]["faces"]
+        try:
+            _, lands = self._detector.fit(frame["gray"], np.array([f.rect for f in faces]))
+        except cv2.error:
+            lands = [None] * len(faces)
+        frame["shapes"]["landmarks"] = tuple(map(Landmarks, lands))
+
+
+class Display(Step):
+    def _op(self, frame):
+        image = frame["image"]
+        for shape in flatten(frame["shapes"].values()):
+            shape.draw(image)
+        cv2.imshow('', image)
+
+    def close(self):
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
