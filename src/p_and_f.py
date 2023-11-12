48a92ce14e1dfe7be9d8fd9c16c4f1245835ea33
Gabriele Lombardi <gabriele.lombardi@argo.vision>
2023-11-12 09:49:50 +0100

    feat: barfi

---
+++ src/p_and_f.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+
+from barfi import st_barfi, barfi_schemas
+import streamlit as st
+
+from tools import Input, Output, define_block
+from shapes import Rect, Landmarks
+
+
+@define_block("Camera", Output("image"))
+def camera():
+    cap = cv2.VideoCapture(0)
+    _, image = cap.read()
+    cap.release()
+    return image
+
+@define_block("EmptyList", Output("list"))
+def empty():
+    return []
+
+@define_block("Rgb2Gray", Input("image"), Output("gray"))
+def rgb2gray(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+@define_block("FaceDetector", Input("gray"), Output("faces"))
+def faces(gray):
+    model = 'haarcascade_frontalface_default.xml'
+    detector = cv2.CascadeClassifier(cv2.data.haarcascades + model)
+    try:
+        res = detector.detectMultiScale(gray, 1.1, 4)
+    except Exception as e:
+        res = []
+    return tuple(map(Rect, res))
+
+@define_block("LandmarksDetector", Input("gray"), Input("faces"), Output("landmarks"))
+def landmarks(gray, faces):
+    detector = cv2.face.createFacemarkLBF()
+    detector.loadModel('models/lbfmodel.yaml')
+    try:
+        _, lands = detector.fit(gray, np.array([f.rect for f in faces]))
+    except cv2.error:
+        lands = [None] * len(faces)
+    return  tuple(map(Landmarks, lands))
+
+@define_block("Concatenate", Input("list1"), Input("list2"), Output("list"))
+def cons(l1, l2):
+    return [] + list(l1) + list(l2)
+
+@define_block("Save image", Input("image"), Input("shapes"))
+def save_image(image, shapes):
+    for shape in shapes:
+        shape.draw(image)
+    cv2.imwrite('generated/frame.png', image)
+
+blocks = [camera, empty, rgb2gray, faces, landmarks, cons, save_image]
+
+load_schema = st.selectbox('Select a saved schema:', barfi_schemas())
+compute_engine = st.checkbox('Activate barfi compute engine', value=False)
+if barfi_result := st_barfi(
+    base_blocks=blocks,
+    load_schema=load_schema,
+    compute_engine=compute_engine,
+    key="face-lands",
+): st.write(barfi_result)
--- src/tools.py
+++ src/tools.py
@@ -1,4 +1,6 @@
 from functools import reduce
+from barfi import Block                                                       
+from dataclasses import dataclass                                             
 
 
 def flat_map(f, xs):
@@ -15,3 +17,39 @@ def compose(*fs):
         return x
 
     return composed
+
+
+@dataclass(frozen=True)
+class Input:
+    name: str
+
+@dataclass(frozen=True)
+class Output:
+    name: str
+
+def define_block(name, *args):
+    def decorator(f):
+        block = Block(name=name)
+        inputs = tuple(filter(lambda a: isinstance(a, Input), args))
+        outputs = tuple(filter(lambda a: isinstance(a, Output), args))
+        for inp in inputs:
+            block.add_input(inp.name)
+        for out in outputs:
+            block.add_output(out.name)
+
+        def feed_func(self):
+            ins = {
+                inp.name: self.get_interface(inp.name)
+                for inp in inputs                                             
+            }
+            outs = f(*ins.values())
+            #outs = f(**ins)
+            if len(outputs) == 1:
+                self.set_interface(name=outputs[0].name, value=outs)
+            elif len(outputs) > 1:
+                for out, v in zip(outputs, outs):
+                    self.set_interface(name=out.name, value=v)
+
+        block.add_compute(feed_func)
+        return block
+    return decorator
