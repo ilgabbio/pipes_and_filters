678baaa0da8db48da4f9dc1dd7145600fc685802
Gabriele Lombardi <gabriele.lombardi@argo.vision>
2023-11-23 09:00:24 +0100

    feat: multiple subgraphs
    NOTE: Stop management missing

--- src/subgraphs.py
+++ src/subgraphs.py
@@ -8,20 +8,8 @@ from pyee.asyncio import AsyncIOEventEmitter
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
-
-_POOL = ThreadPoolExecutor()
-
-
-async def wrap(op, *args): 
-    """
-    Wraps a call to be awaitable using a thread.
-    Useful only if all operations release the GIL.
-    """
-    return await (
-       asyncio
-       .get_event_loop()
-       .run_in_executor(_POOL, partial(op, *args))
-   )
+from collections import deque
+from threading import Thread
 
 
 class Graph:
@@ -30,13 +18,14 @@ class Graph:
             *filters: Tuple[Callable, str | None],
         ) -> None:
         self._sources = []
-        self._emitter = AsyncIOEventEmitter()
+        self._loop = asyncio.new_event_loop()
+        self._emitter = AsyncIOEventEmitter(loop=self._loop)
         for i, (filter, listens) in enumerate(filters):
             emits = filters[i+1][1] if i < len(filters)-1 else None
-            if listens is None:
-                self._sources.append((filter,emits))
-            else:
+            if isinstance(listens, str):
                 self._connect(filter, listens, emits)
+            else:
+                self._sources.append((filter, listens, emits))
 
     def _connect(
             self,
@@ -51,18 +40,48 @@ class Graph:
 
         self._emitter.on(listens, run_process)
 
-    def run(self, stop_condition, data_factory):
+    def run(self, stop_condition, data_factory, new_thread = False):
         async def aloop():
             while not stop_condition():
                 await self._astep(data_factory())
         
-        asyncio.run(aloop())
+        if new_thread:
+            Thread(
+                target=lambda: self._loop.run_until_complete(aloop()),
+                daemon=True,
+            ).start()
+        else:
+            self._loop.run_until_complete(aloop())
 
     async def _astep(self, data):
-        for source, emits in self._sources:
+        async def run_one(source, wait, emits):
             await source(data)
             if emits is not None:
                 self._emitter.emit(emits, data)
+
+        for s in self._sources:
+            must_wait = s[1]
+            if must_wait:
+                await run_one(*self._sources[-1])
+            else:
+                asyncio.create_task(run_one(*s))
+
+
+_POOL = ThreadPoolExecutor()
+async def wrap(op, *args): 
+    """
+    Wraps a call to be awaitable using a thread.
+    Useful only if all operations release the GIL.
+    """
+    try:
+        return await (
+            asyncio
+                .get_event_loop()
+                .run_in_executor(_POOL, partial(op, *args))
+        )
+    except RuntimeError:
+        # Loop stopped:
+        pass
 
 
 async def source(frame):
@@ -117,28 +136,65 @@ async def faces_landmarks(frame):
 
 
 async def display(frame):
-    image = frame["image"]
+    image = frame.get("image")
+    if image is None:
+        return
     for shape in flatten(frame["shapes"].values()):
         shape.draw(image)
     cv2.imshow('', image)
 
 
+def push(queue: deque):
+    async def pusher(data):
+        queue.append(data)
+    return pusher
+
+def pull(queue: deque, is_stop_required=None, wait=True):
+    async def puller(data):
+        if not queue and not wait:
+            return
+        while not queue:
+            if is_stop_required and is_stop_required():
+                return
+            await asyncio.sleep(.1)
+        val = queue.pop()
+        data.clear()
+        data.update(val)
+    return puller
+
+
 def main():
-    # Creating a graph for asynchronous processing:
-    graph = Graph(
-        (source, None),
-        (preprocess, 'image'),
-        (faces_detector, 'gray'),
-        (faces_landmarks, 'faces'),
-        (display, 'landmarks'),
-    )
+    # Context data:
+    ctx = {}
+    must_stop = False
+    is_stop_required = lambda: must_stop
+    def check_stop():
+        nonlocal must_stop
+        return (must_stop := cv2.waitKey(1) == 27)
+
+    # A pipeline with two graphs:
+    q1 = deque(maxlen=5)
+    q2 = deque(maxlen=5)
+    g1 = Graph(
+            (pull(q2, wait=False), False),
+            (display, 'drawn'),
+            (source, True),
+            (preprocess, 'image'),
+            (faces_detector, 'gray'),
+            (push(q1), 'faces'),
+        )
+    g2 = Graph(
+            (pull(q1), True),
+            (faces_landmarks, 'faces'),
+            (push(q2), 'landmarks'),
+        )
 
     # This run cannot guarantee the frame ordering:
-    ctx = {}
-    graph.run(
-        lambda: cv2.waitKey(1) == 27,
-        lambda: {"ctx": ctx, "shapes": {}}
-    )
+    g2.run(is_stop_required, lambda: {}, new_thread=True)
+    g1.run(check_stop, lambda: {"ctx": ctx, "shapes": {}})
+
+    # Brutal, stop to be revisited:
+    raise SystemExit
 
 if __name__ == "__main__":
     main()
